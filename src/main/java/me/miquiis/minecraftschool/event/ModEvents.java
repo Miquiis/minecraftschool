75d9f1a990e961fa3bb43fbc41ddd6768d4b9729
Mateus DaCunha <59839289+Miquiis@users.noreply.github.com>
2021-11-16 08:48:32 -0600

    First Commit

---
+++ src/main/java/me/miquiis/minecraftschool/event/ModEvents.java
@@ -0,0 +1,19 @@
+package me.miquiis.minecraftschool.event;
+
+import me.miquiis.minecraftschool.MinecraftSchool;
+import me.miquiis.minecraftschool.entity.ModEntityTypes;
+import me.miquiis.minecraftschool.entity.custom.FakePlayerEntity;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = MinecraftSchool.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModEvents {
+
+    @SubscribeEvent
+    public static void addEntityAttributes(EntityAttributeCreationEvent event)
+    {
+        event.put(ModEntityTypes.FAKE_PLAYER.get(), FakePlayerEntity.setCustomAttributes().create());
+    }
+
+}
