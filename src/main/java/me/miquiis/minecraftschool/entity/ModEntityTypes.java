4c01cb10e98ec8369cdd88213746230b1db87bcb
Miquiis <miquiisoficial@gmail.com>
2021-11-16 08:44:11 -0600

    first commit

---
+++ src/main/java/me/miquiis/minecraftschool/entity/ModEntityTypes.java
@@ -0,0 +1,31 @@
+package me.miquiis.minecraftschool.entity;
+
+import me.miquiis.minecraftschool.MinecraftSchool;
+import me.miquiis.minecraftschool.entity.custom.FakePlayerEntity;
+import net.minecraft.entity.EntityClassification;
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.util.FakePlayer;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModEntityTypes {
+
+    public static DeferredRegister<EntityType<?>> ENTITY_TYPES
+            = DeferredRegister.create(ForgeRegistries.ENTITIES, MinecraftSchool.MOD_ID);
+
+    public static final RegistryObject<EntityType<FakePlayerEntity>> FAKE_PLAYER =
+            ENTITY_TYPES.register("fake_player",
+                    () -> EntityType.Builder.create(FakePlayerEntity::new, EntityClassification.CREATURE)
+                    .size(0.6f, 2f)
+                    .build(new ResourceLocation(MinecraftSchool.MOD_ID, "fake_player").toString())
+            );
+
+    public static void register(IEventBus eventBus)
+    {
+        ENTITY_TYPES.register(eventBus);
+    }
+
+}
---
+++ src/main/java/me/miquiis/minecraftschool/entity/custom/FakePlayerEntity.java
@@ -0,0 +1,23 @@
+package me.miquiis.minecraftschool.entity.custom;
+
+import net.minecraft.entity.CreatureEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.ai.attributes.AttributeModifierMap;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.world.World;
+
+public class FakePlayerEntity extends CreatureEntity {
+    public FakePlayerEntity(EntityType<? extends CreatureEntity> type, World worldIn) {
+        super(type, worldIn);
+    }
+
+    public static AttributeModifierMap.MutableAttribute setCustomAttributes() {
+        return MobEntity.func_233666_p_()
+                .createMutableAttribute(Attributes.MAX_HEALTH, 20.0D)
+                .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.33D)
+                .createMutableAttribute(Attributes.ATTACK_DAMAGE, 5.0D)
+                .createMutableAttribute(Attributes.FOLLOW_RANGE, 50.0D);
+    }
+
+}
